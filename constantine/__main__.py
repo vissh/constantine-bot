3da68c61514a1fa0dd97aebd33e706cad17f387b
vissh <unsumulum@gmail.com>
2016-07-22 11:37:06 +0000

    Удален вечный цикл

--- constantine/__main__.py
+++ constantine/__main__.py
@@ -14,22 +14,13 @@ token = config['main']['bot_token']
 jenkins_url = config['main']['jenkins_url']
 jobs_names = dict(config['jobs'])
 
-jenkins_username = input('jenkins username:')
-jenkins_password = getpass.getpass(prompt='jenkins password:')
+if len(sys.argv) == 3:
+    _, jenkins_username, jenkins_password = sys.argv
+else:
+    jenkins_username = input('jenkins username:')
+    jenkins_password = getpass.getpass(prompt='jenkins password:')
 
-while True:
-    monsieur = Constantine(token, jenkins_url, jenkins_username,
-                           jenkins_password, jobs_names)
-    try:
-        monsieur.wake_up()
-    except Exception as ex:
-        monsieur.bot.stop_polling()
-        monsieur.bot.worker_pool.close()
-        sys.stdout.write('\n\n\n' + '=' * 42 + '\n')
-        sys.stdout.write('{}\n'.format(datetime.datetime.now()))
-        traceback.print_exception(*sys.exc_info())
-        sys.stdout.write('\n' + str(ex))
-        sys.stdout.write('\nRestart loop!\n' + '=' * 42 + '\n')
-        time.sleep(5)
-    else:
-        break
+monsieur = Constantine(token, jenkins_url, jenkins_username,
+                       jenkins_password, jobs_names)
+monsieur.wake_up()
+
